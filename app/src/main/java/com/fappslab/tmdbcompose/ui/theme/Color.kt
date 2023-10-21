8203b223fe56279a8e5b55c05323674de71f8b9e
F4bioo <costa.fbo@gmail.com>
2023-10-21 16:17:09 -0300

    Added: Setup gradle dependencies

--- app/src/main/java/com/fappslab/tmdbcompose/ui/theme/Color.kt
+++ app/src/main/java/com/fappslab/tmdbcompose/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.fappslab.tmdbcompose.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val BlackPrimary = Color(0xFF000000)
+val YellowSecondary = Color(0xFFFFAC33)
+val BlackSecondary = Color(0xFF000000)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val black = Color(0xFF000000)
+val yellow = Color(0xFFFFAC33)
+val white = Color(0xFFFFFFFF)
