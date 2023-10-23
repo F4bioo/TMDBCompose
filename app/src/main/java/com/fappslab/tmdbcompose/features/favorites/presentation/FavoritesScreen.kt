5a1a9b415420047f442170c020d6acdaabd3b3a3
F4bioo <costa.fbo@gmail.com>
2023-10-23 20:22:33 -0300

    Added: Create favorites presentation layer

---
+++ app/src/main/java/com/fappslab/tmdbcompose/features/favorites/presentation/FavoritesScreen.kt
@@ -0,0 +1,9 @@
+package com.fappslab.tmdbcompose.features.favorites.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun Favorites(navHostController: NavHostController) {
+
+}
