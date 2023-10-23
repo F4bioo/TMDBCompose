7ad557eb65516f93b1015a1c33e9f8bb9eb5307d
F4bioo <costa.fbo@gmail.com>
2023-10-23 20:18:31 -0300

    Added: Create detail data layer

---
+++ app/src/main/java/com/fappslab/tmdbcompose/features/detail/data/paging/SimilarMoviesPagingSource.kt
@@ -0,0 +1,38 @@
+package com.fappslab.tmdbcompose.features.detail.data.paging
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.fappslab.tmdbcompose.core.data.constant.Common.FETCH_LIMIT
+import com.fappslab.tmdbcompose.core.data.model.extension.toMovies
+import com.fappslab.tmdbcompose.core.domain.model.Movie
+import com.fappslab.tmdbcompose.features.detail.data.source.remote.DetailRemoteDataSource
+
+class SimilarMoviesPagingSource(
+    private val id: Int,
+    private val remoteDataSource: DetailRemoteDataSource
+) : PagingSource<Int, Movie>() {
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Movie> {
+        return runCatching {
+            val page = params.key ?: 1
+            val response = remoteDataSource.getSimilarMovies(page, id)
+            val movies = response.result
+
+            LoadResult.Page(
+                data = movies.toMovies(),
+                prevKey = if (page == 1) null else page.dec(),
+                nextKey = if (movies.isNullOrEmpty()) null else page.inc()
+            )
+
+        }.getOrElse {
+            LoadResult.Error(throwable = it)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, Movie>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?.prevKey?.plus(FETCH_LIMIT) ?: anchorPage?.nextKey?.minus(FETCH_LIMIT)
+        }
+    }
+}
