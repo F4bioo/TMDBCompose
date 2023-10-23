0c370a1ef31e26d8d5519cde5e730bccf71311da
F4bioo <costa.fbo@gmail.com>
2023-10-23 20:19:26 -0300

    Added: Create detail dependency injection

---
+++ app/src/main/java/com/fappslab/tmdbcompose/features/detail/di/DetailModule.kt
@@ -0,0 +1,40 @@
+package com.fappslab.tmdbcompose.features.detail.di
+
+import androidx.paging.PagingConfig
+import com.fappslab.tmdbcompose.core.data.api.MovieService
+import com.fappslab.tmdbcompose.features.detail.data.repository.DetailRepositoryImpl
+import com.fappslab.tmdbcompose.features.detail.data.source.remote.DetailRemoteDataSource
+import com.fappslab.tmdbcompose.features.detail.data.source.remote.DetailRemoteDataSourceImpl
+import com.fappslab.tmdbcompose.features.detail.domain.repository.DetailRepository
+import com.fappslab.tmdbcompose.features.detail.domain.usecase.GetMovieDetailUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DetailModule {
+
+    @Provides
+    fun provideDetailRemoteDataSource(
+        service: MovieService
+    ): DetailRemoteDataSource {
+        return DetailRemoteDataSourceImpl(service)
+    }
+
+    @Provides
+    fun provideDetailRepository(
+        remoteDataSource: DetailRemoteDataSource,
+        config: PagingConfig
+    ): DetailRepository {
+        return DetailRepositoryImpl(remoteDataSource, config)
+    }
+
+    @Provides
+    fun provideGetMovieDetailUseCase(
+        repository: DetailRepository
+    ): GetMovieDetailUseCase {
+        return GetMovieDetailUseCase(repository)
+    }
+}
