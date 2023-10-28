8668b78f09a444ac13c819a92e5e05aff7d0e9b6
F4bioo <costa.fbo@gmail.com>
2023-10-28 18:45:52 -0300

    Added: Create buildSrc

---
+++ buildSrc/src/main/java/Modules.kt
@@ -0,0 +1,17 @@
+object Modules {
+    // Core
+    const val local = ":core:data:local"
+    const val remote = ":core:data:remote"
+    const val domain = ":core:domain"
+    const val navigation = ":core:navigation"
+
+    // Features
+    const val detail = ":features:detail"
+    const val favorite = ":features:favorite"
+    const val popular = ":features:popular"
+    const val search = ":features:search"
+
+    // Libraries
+    const val arch = ":libraries:arch"
+    const val design = ":libraries:design"
+}
